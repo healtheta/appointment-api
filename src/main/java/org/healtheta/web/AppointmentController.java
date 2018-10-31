fc1975f2ac9d39e23444bbd62aaa7651ca944392
pjeyarat <pragash.jeyaratnam@gmail.com>
2018-10-31 15:31:33 +0530

    added edge-server support

--- src/main/java/org/healtheta/web/AppointmentController.java
+++ src/main/java/org/healtheta/web/AppointmentController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/appointment")
 public class AppointmentController {
     @Autowired
     AppointmentRepo appointmentRepo;
